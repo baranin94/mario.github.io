ed5256aa297056623cdd3bced36b63fe53c87a36
Mario Vasovic <50682101+baranin94@users.noreply.github.com>
2019-11-13 12:55:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Buttons
+function sendBtn() {
+    const mail = document.getElementById('send-btn');
+    window.open('https://mail.google.com/mail/u/0/#inbox?compose=new', '_blank');
+}
+
+function openMyRasume() {
+    const rasume = document.getElementById('myRasume');
+    window.open('https://resume.io/r/ox1tQdFT6', '_blank');
+}
